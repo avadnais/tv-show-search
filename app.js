c9197e8baf4ed90701e0521ad0819210a26005fd
Andy <avadnais@oswego.edu>
2022-07-26 16:33:52 -0600

    adding summary and styling

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const searchTerm = form.elements.query.value;
-    const config = { params: { q: searchTerm } }
-    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
-    removePrevResults()
-    makeImages(res.data)
-    form.elements.query.value = '';
-})
+form.addEventListener('submit', async function(e) {
+	e.preventDefault();
+	const searchTerm = form.elements.query.value;
+	const config = { params: { q: searchTerm } };
+	const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+	removePrevResults();
+	makeResults(res.data);
+	form.elements.query.value = '';
+});
 
-const makeImages = (shows) => {
-
-    if(shows.length == 0) {
-        const noResText = document.createElement('h4')
-        noResText.id = "noResText"
-        noResText.innerHTML = "No Results Found"
-        document.body.append(noResText)
-    }
-    for (let result of shows) {
-        if (result.show.image) {
-            const img = document.createElement('IMG');
-            img.src = result.show.image.medium;
-            document.body.append(img)
-        }
-    }
-}
+const makeResults = (shows) => {
+	if (shows.length == 0) {
+		const noResText = document.createElement('h4');
+		noResText.id = 'noResText';
+		noResText.innerHTML = 'No Results Found';
+		document.body.append(noResText);
+	}
+	for (let result of shows) {
+		if (result.show.image) {
+			console.log(result.show);
+			makeShowDiv(result.show);
+		}
+	}
+};
 
 const removePrevResults = () => {
-    const noResText = document.querySelector('#noResText')
-    noResText && noResText.remove();
-    const imgs = document.querySelectorAll('img')
-    imgs.forEach(img => img.remove())
+	const noResText = document.querySelector('#noResText');
+	noResText && noResText.remove();
+	const shows = document.querySelectorAll('.show');
+	shows.forEach((show) => show.remove());
+};
+
+function makeShowDiv(s) {
+	const showDiv = document.createElement('div');
+	showDiv.classList.add('show');
+	showDiv.innerHTML = `
+        <img src=${s.image.original}></img>
+        <h4>${s.name}</h4>
+        ${s.summary}
+        <div class='genre-container'>
+        ${s.genres
+			.map((g) => {
+				return `<span class="genre">${g}</span>`;
+			})
+			.join('')}
+        </div>
+        
+        ${s.officialSite ? `<h6 class="watchNow"><a href="${s.officialSite}">Watch now</h6>` : ``}
+        
+
+        
+    `;
+
+	document.querySelector('.results-container').append(showDiv);
 }
