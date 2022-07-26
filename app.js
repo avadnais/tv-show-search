2626aa6e7721b9fb9ba2ec3bbd5f65a9df251749
Andy <avadnais@oswego.edu>
2022-07-25 22:46:35 -0600

    refactor removing prev results

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ form.addEventListener('submit', async function (e) {
     const searchTerm = form.elements.query.value;
     const config = { params: { q: searchTerm } }
     const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
-    removeImages()
+    removePrevResults()
     makeImages(res.data)
     form.elements.query.value = '';
 })
 
 const makeImages = (shows) => {
+
+    if(shows.length == 0) {
+        const noResText = document.createElement('h4')
+        noResText.id = "noResText"
+        noResText.innerHTML = "No Results Found"
+        document.body.append(noResText)
+    }
     for (let result of shows) {
         if (result.show.image) {
             const img = document.createElement('IMG');
@@ -19,7 +26,9 @@ const makeImages = (shows) => {
     }
 }
 
-const removeImages = () => {
+const removePrevResults = () => {
+    const noResText = document.querySelector('#noResText')
+    noResText && noResText.remove();
     const imgs = document.querySelectorAll('img')
     imgs.forEach(img => img.remove())
 }
